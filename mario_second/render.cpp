3bb7d43f49f0ef39090d05c4d11b0490a16795f3
dariabeaufort <zdavydova10@gmail.com>
2026-05-21 00:56:58 +0300

    [mario_second] refactor render module

--- mario_second/render.cpp
+++ mario_second/render.cpp
@@ -1,68 +1,64 @@
-#include <cstdio>
 #include <cmath>
+#include <cstdio>
+#include <cstring>
+
 #include <windows.h>
 
-#include "render.hpp"
-#include "object.hpp"
 #include "game_state.hpp"
+#include "object.hpp"
 
-using jbeau::state::map;
-using jbeau::state::score;
-using jbeau::object::Object;
+#include "render.hpp"
 
-namespace jbeau {
-	namespace render {
-		bool is_pos_in_map(int x, int y) {
-			return ((x >= 0) && (x < jbeau::state::MAP_WIDTH) && (y >= 0) && (y < jbeau::state::MAP_HEIGHT));
-		}
-		
-		void clear_map() {
-			for (int i = 0; i < jbeau::state::MAP_WIDTH; i++) {
-				map[0][i] = ' ';
-			}
-			map[0][jbeau::state::MAP_WIDTH] = '\0';
-			
-			for (int j = 1; j < jbeau::state::MAP_HEIGHT; j++) {
-				sprintf(map[j], map[0]);
-			}
-		}
+bool jbeau::render::is_pos_in_map(int x, int y) {
+	return ((x >= 0) && (x < jbeau::state::MAP_WIDTH) 
+		   && (y >= 0) && (y < jbeau::state::MAP_HEIGHT));
+}
 
-		void show_map() {
-			map[jbeau::state::MAP_HEIGHT - 1][jbeau::state::MAP_WIDTH - 1] = '\0';
-			for (int j = 0; j < jbeau::state::MAP_HEIGHT; j++) {
-				printf("%s", map[j]);
-			}
-		}
+void jbeau::render::clear_map() {
+	for (int i = 0; i < jbeau::state::MAP_WIDTH; i++) {
+		jbeau::state::map[0][i] = ' ';
+	}
+	jbeau::state::map[0][jbeau::state::MAP_WIDTH] = '\0';
+	
+	for (int j = 1; j < jbeau::state::MAP_HEIGHT; j++) {
+		sprintf(jbeau::state::map[j], jbeau::state::map[0]);
+	}
+}
 
-		void put_object_on_map(Object obj) {
-			int ix = static_cast<int>(round(obj.x));
-			int iy = static_cast<int>(round(obj.y));
-			int i_width = static_cast<int>(round(obj.width));
-			int i_height = static_cast<int>(round(obj.height));
-			
-			for (int i = ix; i < (ix + i_width); i++) {
-				for (int j = iy; j < (iy + i_height); j++) {
-					if (is_pos_in_map(i, j)) {
-						map[j][i] = obj.c_type;
-					}
-				}
+void jbeau::render::show_map() {
+	jbeau::state::map[jbeau::state::MAP_HEIGHT - 1][jbeau::state::MAP_WIDTH - 1] = '\0';
+	for (int j = 0; j < jbeau::state::MAP_HEIGHT; j++) {
+		printf("%s", jbeau::state::map[j]);
+	}
+}
+
+void jbeau::render::put_object_on_map(const jbeau::object::Object &obj) {
+	int ix = static_cast<int>(round(obj.x));
+	int iy = static_cast<int>(round(obj.y));
+	int i_width = static_cast<int>(round(obj.width));
+	int i_height = static_cast<int>(round(obj.height));
+	
+	for (int i = ix; i < (ix + i_width); i++) {
+		for (int j = iy; j < (iy + i_height); j++) {
+			if (jbeau::render::is_pos_in_map(i, j)) {
+				jbeau::state::map[j][i] = obj.c_type;
 			}
 		}
+	}
+}
 
-		void set_cur(int x, int y) {
-			COORD coord;
-			coord.X = x;
-			coord.Y = y;
-			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-		}
+void jbeau::render::set_cur(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
-		void put_score_on_map() {
-			char score_text[30];
-			sprintf(score_text, "Score: %d", score);
-			const int len = strlen(score_text);
-			for (int i = 0; i < len; i++) {
-				map[jbeau::state::SCORE_TEXT_Y][i + jbeau::state::SCORE_TEXT_X] = score_text[i];
-			}
-		}
+void jbeau::render::put_score_on_map() {
+	char score_text[30];
+	sprintf(score_text, "Score: %d", jbeau::state::score);
+	const int len = std::strlen(score_text);
+	for (int i = 0; i < len; i++) {
+		jbeau::state::map[jbeau::state::SCORE_TEXT_Y][i + jbeau::state::SCORE_TEXT_X] = score_text[i];
 	}
 }
--- mario_second/render.hpp
+++ mario_second/render.hpp
@@ -8,7 +8,7 @@ namespace jbeau {
 		void clear_map();
 		void show_map();
 		void set_cur(int x, int y);
-		void put_object_on_map(jbeau::object::Object obj);
+		void put_object_on_map(const jbeau::object::Object &obj);
 		void put_score_on_map();
 	}
 }
