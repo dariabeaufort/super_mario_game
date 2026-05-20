7277e50e25f40c5b759c484a2a4e1413164e418f
dariabeaufort <zdavydova10@gmail.com>
2026-05-21 01:14:59 +0300

    [mario_second] refactor movement and level modules

--- mario_second/level.cpp
+++ mario_second/level.cpp
@@ -2,48 +2,29 @@
 
 #include <windows.h>
 
-#include "level.hpp"
-
 #include "game_state.hpp"
 #include "object.hpp"
 
-using jbeau::object::init_object;
+#include "level.hpp"
 
-using jbeau::state::brick;
-using jbeau::state::brick_length;
+using jbeau::object::init_object;
 
 using jbeau::state::get_new_brick;
 using jbeau::state::get_new_moving;
 
-using jbeau::state::level;
-
-using jbeau::state::mario;
-
-using jbeau::state::max_lvl;
-
-using jbeau::state::moving;
-using jbeau::state::moving_length;
-
-using jbeau::state::score;
-
-// temporary dependencies (to be refactored later)
-static const char BONUS_BRICK_SYMBOL = '?';
-static const char BRICK_SYMBOL = '#';
-static const char EMPTY_BRICK_SYMBOL = '-';
-static const char ENEMY_SYMBOL = 'o';
-static const char FINISH_SYMBOL = '+';
-static const char PLAYER_SYMBOL = '@';
-
 void jbeau::level::create_level(int lvl) {
 	system("color DF");
 	
-	brick_length = 0;
-	brick = static_cast<jbeau::object::Object*>(realloc(brick, 0));
-	moving_length = 0;
-	moving = static_cast<jbeau::object::Object*>(realloc(moving, 0));
+	jbeau::state::brick_length = 0;
+	jbeau::state::brick = static_cast<jbeau::object::Object*>(
+						  realloc(jbeau::state::brick, 0));
+
+	jbeau::state::moving_length = 0;
+	jbeau::state::moving = static_cast<jbeau::object::Object*>(
+						   realloc(jbeau::state::moving, 0));
 	
-	init_object(&mario, 39, 10, 3, 3, jbeau::state::PLAYER_SYMBOL);
-	score = 0;
+	init_object(&jbeau::state::mario, 39, 10, 3, 3, jbeau::state::PLAYER_SYMBOL);
+	jbeau::state::score = 0;
 	
 	if (lvl == 1) {
 		init_object(get_new_brick(), 20, 20, 40, 5, jbeau::state::BRICK_SYMBOL);
@@ -94,5 +75,6 @@ void jbeau::level::create_level(int lvl) {
 		init_object(get_new_moving(), 120, 10, 3, 2, jbeau::state::ENEMY_SYMBOL);
 		init_object(get_new_moving(), 130, 10, 3, 2, jbeau::state::ENEMY_SYMBOL);
 	}
-	max_lvl = 3;
+	
+	jbeau::state::max_lvl = 3;
 }
--- mario_second/movement.cpp
+++ mario_second/movement.cpp
@@ -1,61 +1,42 @@
-#include <stdlib.h>
+#include <cstdlib>
 
 #include <windows.h>
-
-#include "movement.hpp"
 
 #include "collision.hpp"
 #include "game_state.hpp"
 #include "level.hpp"
 #include "object.hpp"
 
-using jbeau::collision::is_collision;
-
-using jbeau::level::create_level;
-
-using jbeau::object::init_object;
-using jbeau::object::Object;
-using jbeau::object::set_object_pos;
+#include "movement.hpp"
 
 using jbeau::state::brick;
-using jbeau::state::brick_length;
-
-using jbeau::state::map;
-using jbeau::state::MAP_HEIGHT;
-
-using jbeau::state::mario;
-
-using jbeau::state::max_lvl;
-
 using jbeau::state::moving;
-using jbeau::state::moving_length;
-
-using jbeau::state::get_new_moving;
-
 
 void jbeau::movement::player_dead() {
 	system("color 4F");
 	Sleep(jbeau::state::DEAD_SCREEN_TIME_MS);
-	create_level(jbeau::state::level);
+	jbeau::level::create_level(jbeau::state::level);
 }
 
-void jbeau::movement::vert_move_object(Object *obj) {
+void jbeau::movement::vert_move_object(jbeau::object::Object *obj) {
 	obj->is_fly = true;
 	obj->vert_speed += jbeau::state::GRAVITY;
-	set_object_pos(obj, obj->x, obj->y + obj->vert_speed);
+	jbeau::object::set_object_pos(obj, obj->x, obj->y + obj->vert_speed);
 	
-	for (int i = 0; i < brick_length; i++) {
-		if (is_collision(*obj, brick[i])) {
+	for (int i = 0; i < jbeau::state::brick_length; i++) {
+		if (jbeau::collision::is_collision(*obj, brick[i])) {
 			if (obj->vert_speed > 0) {
 				obj->is_fly = false;
 			}
 			
 			if ((brick[i].c_type == jbeau::state::BONUS_BRICK_SYMBOL) 
 				&& (obj->vert_speed < 0) 
-			    && (obj == &mario)) {
+			    && (obj == &jbeau::state::mario)) {
 				brick[i].c_type = jbeau::state::EMPTY_BRICK_SYMBOL;
-				init_object(get_new_moving(), brick[i].x, brick[i].y - 3, 3, 2, jbeau::state::MONEY_SYMBOL);
-				moving[moving_length - 1].vert_speed = jbeau::state::MOVING_VERT_SPEED;
+				jbeau::object::init_object(jbeau::state::get_new_moving(), 
+										   brick[i].x, brick[i].y - 3, 3, 2, 
+										   jbeau::state::MONEY_SYMBOL);
+				moving[jbeau::state::moving_length - 1].vert_speed = jbeau::state::MOVING_VERT_SPEED;
 			}
 			
 			obj->y -= obj->vert_speed;
@@ -64,24 +45,24 @@ void jbeau::movement::vert_move_object(Object *obj) {
 			if (brick[i].c_type == jbeau::state::FINISH_SYMBOL) {
 				jbeau::state::level++;
 				
-				if (jbeau::state::level > max_lvl) {
+				if (jbeau::state::level > jbeau::state::max_lvl) {
 					jbeau::state::level = 1;
 				}
 				
 				system("color 2F");
 				Sleep(jbeau::state::LVL_SWITCH_TIME_MS);
-				create_level(jbeau::state::level);
+				jbeau::level::create_level(jbeau::state::level);
 			}
 			break;
 		}
 	}
 }
 
-void jbeau::movement::horizon_move_object(Object *obj) {
+void jbeau::movement::horizon_move_object(jbeau::object::Object *obj) {
 	obj->x += obj->horiz_speed;
 	
-	for (int i = 0; i < brick_length; i++) { 
-		if (is_collision(*obj, brick[i])) {
+	for (int i = 0; i < jbeau::state::brick_length; i++) { 
+		if (jbeau::collision::is_collision(*obj, brick[i])) {
 			obj->x -= obj->horiz_speed;
 			obj->horiz_speed = -obj->horiz_speed;
 			return;
@@ -89,7 +70,7 @@ void jbeau::movement::horizon_move_object(Object *obj) {
 	}
 	
 	if (obj->c_type == jbeau::state::ENEMY_SYMBOL) {
-		Object tmp_obj = *obj;
+		jbeau::object::Object tmp_obj = *obj;
 		jbeau::movement::vert_move_object(&tmp_obj);
 		
 		if (tmp_obj.is_fly == true) {
@@ -100,20 +81,20 @@ void jbeau::movement::horizon_move_object(Object *obj) {
 }
 
 void jbeau::movement::horizon_move_map(float dx) {
-	mario.x -= dx;
-	for (int i = 0; i < brick_length; i++) {
-		if (is_collision(mario, brick[i])) {
-			mario.x += dx;
+	jbeau::state::mario.x -= dx;
+	for (int i = 0; i < jbeau::state::brick_length; i++) {
+		if (jbeau::collision::is_collision(jbeau::state::mario, brick[i])) {
+			jbeau::state::mario.x += dx;
 			return;
 		}
 	}
-	mario.x += dx;
+	jbeau::state::mario.x += dx;
 	
-	for (int i = 0; i < brick_length; i++) {
+	for (int i = 0; i < jbeau::state::brick_length; i++) {
 		brick[i].x += dx;
 	}
 	
-	for (int i = 0; i < moving_length; i++) {
+	for (int i = 0; i < jbeau::state::moving_length; i++) {
 		moving[i].x += dx;
 	}
 }
